8b832186d08c266b0e7f904b1e2b64ecfc867738
Your Name <renukaavinash@gmail.com>
2025-07-01 16:59:21 +0530

    SB and IRM screens done

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
